a9addd2ebd71bdfd94227f6c7505352ead39e2bf
なるみ <weaper@gmail.com>
2018-08-29 12:23:20 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import find_packages, setup
 setup(
     name='labelmeutils',
     version='0.1.0',
-    packages=find_packages,
+    packages=find_packages(),
     install_requires=[])
