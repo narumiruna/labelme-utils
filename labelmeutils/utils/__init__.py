007cbea9fde0a5e9dcb957262a5b9c7d70b51b09
なるみ <weaper@gmail.com>
2018-08-29 12:04:32 +0800

    Add tf record utils

--- labelmeutils/utils/__init__.py
+++ labelmeutils/utils/__init__.py
@@ -1,1 +1,2 @@
 from .io import load_image, load_json, save_json
+from .record import bytes_feature, float_feature, int64_feature
---
+++ labelmeutils/utils/record.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
